07be240bda5b3ef05d38c45b7820f1e88e734e43
vivek <vivek@athenaeum.in>
2021-04-26 17:04:56 +0530

    state change in props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,27 @@ import Sample from './components/Sample.jsx';
 import Demo from './components/Demo';
 
 class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  increment = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
   render() {
     return (
       <>
-        <h3>App Component</h3>
+        <h3>App Component: {this.state.counter}</h3>
         <hr />
-        <Sample message='Good evening' city='Bengaluru' />
+        <Sample
+          message='Good evening'
+          city='Bengaluru'
+          counter={this.state.counter}
+        />
+
+        <hr />
+        <button onClick={this.increment}>Increment</button>
       </>
     );
   }
