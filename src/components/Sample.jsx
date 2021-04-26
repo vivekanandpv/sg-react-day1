256313a6802953b0e45f277afa7b65035ad00ab0
vivek <vivek@athenaeum.in>
2021-04-26 17:15:33 +0530

    child to parent in class component

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
+// import React from 'react';
 
-const Sample = (props) => {
-  return (
-    <>
-      <h3>
-        I am Sample component {props.message} : Counter {props.counter}
-      </h3>
-      <p>City: {props.city}</p>
-      <hr />
-      <button onClick={() => props.inc(3)}>Increment</button>
-    </>
-  );
-};
-
-export default Sample;
-
-// import React, { Component } from 'react';
-
-// class Sample extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         <h3>I am Sample component {this.props.message}</h3>
-//         <p>City: {this.props.city}</p>
-//       </>
-//     );
-//   }
-// }
+// const Sample = (props) => {
+//   return (
+//     <>
+//       <h3>
+//         I am Sample component {props.message} : Counter {props.counter}
+//       </h3>
+//       <p>City: {props.city}</p>
+//       <hr />
+//       <button onClick={() => props.inc(3)}>Increment</button>
+//     </>
+//   );
+// };
 
 // export default Sample;
+
+import React, { Component } from 'react';
+
+class Sample extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <h3>
+          I am Sample component {this.props.message} : Counter{' '}
+          {this.props.counter}
+        </h3>
+        <p>City: {this.props.city}</p>
+        <hr />
+        <button onClick={() => this.props.inc(3)}>Increment</button>
+      </>
+    );
+  }
+}
+
+export default Sample;
